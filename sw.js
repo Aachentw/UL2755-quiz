851b97400340224c1ab4fa7554bfcf5902ed9971
Aachentw <chen.lihsiu@gmail.com>
2026-04-22 13:47:17 +0800

    Full question bank: 237 Qs (144 UL2755 + 22 NEC 646 + 3 NEC 110.26 + 68 FXX) + 711 nova TTS MP3s (71 MB, $1.52)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ul2755-v17';
+const CACHE = 'ul2755-v18';
 const SHELL = ['./', './index.html', './app.js', './srs.js', './manifest.webmanifest', './questions.json', './version.json'];
 
 self.addEventListener('install', (e) => {
